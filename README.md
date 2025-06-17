da4d1cffc6421e2449ef14976159d4dd9d7eaf77
Jeremy Chone <jeremy.chone@gmail.com>
2025-06-17 16:44:19 -0700

    . c01-simple - simple get

---
+++ README.md
@@ -0,0 +1,11 @@
+
+
+## ValKey server
+
+```sh
+docker run --rm -p 6379:6379 valkey/valkey:8.1.2 
+``` 
+
+<br />
+
+[This repo](https://github.com/jeremychone-channel/rust-xp-valkey-stream)
--- examples/c01-simple.rs
+++ examples/c01-simple.rs
@@ -1,6 +1,20 @@
+use redis::Commands;
+
 #[tokio::main]
 async fn main() -> core::result::Result<(), Box<dyn std::error::Error>> {
 	println!("Hello, world!");
+
+	let client = redis::Client::open("redis://127.0.0.1:6379")?;
+	let mut con = client.get_connection()?;
+	// throw away the result, just make sure it does not fail
+	let _: () = con.set("my_key", 42)?;
+
+	// read back the key and return it.  Because the return value
+	// from the function is a result for integer this will automatically
+	// convert into one.
+	let res: i32 = con.get("my_key")?;
+
+	println!("->> {res:?}");
 
 	Ok(())
 }
